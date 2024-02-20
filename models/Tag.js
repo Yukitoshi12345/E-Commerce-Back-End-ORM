546f7bca9b5c8281c8b1f776599034428d2aefbb
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-20 17:12:07 +1100

    Tag.js completed

--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,38 @@
+// Import the Sequelize Model and DataTypes objects from the 'sequelize' library
 const { Model, DataTypes } = require('sequelize');
 
+// Import the sequelize instance from the '../config/connection.js' file
 const sequelize = require('../config/connection.js');
 
+// Define the Tag model using the Sequelize Model class
 class Tag extends Model {}
 
+// Initialise the Tag model with the following properties:
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
+    // Associate the model with the sequelize instance
     sequelize,
+    // Disable timestamps for this model
     timestamps: false,
+    // Prevent Sequelize from pluralising the table name
     freezeTableName: true,
+    // Use underscores instead of camelCase for field names
     underscored: true,
+    // Set the model name to 'tag'
     modelName: 'tag',
   }
 );
 
+// Export the Tag model for use in other parts of the application
 module.exports = Tag;
