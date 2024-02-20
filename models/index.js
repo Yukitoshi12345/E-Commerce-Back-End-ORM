f7df7db14d23e6496ff7e6484b4f7f1ab697f5e6
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-20 17:24:04 +1100

    index.js in models completed

--- models/index.js
+++ models/index.js
@@ -1,17 +1,36 @@
-// import models
+// Import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Define associations between models using Sequelize methods
+
 // Products belongsTo Category
+// Belongs to one **Category** model (one-to-many relationship)
+Product.belongsTo(Category);
 
 // Categories have many Products
+// Has many **Product** models (one-to-many relationship)
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+// Belongs to many **Tag** models (many-to-many relationship)
+// The relationship is established through the **ProductTag** model
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+// Belongs to many **Product** models (many-to-many relationship)
+// The relationship is established through the **ProductTag** model which acts as a join table
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
+// Export all models for use in other parts of your application
 module.exports = {
   Product,
   Category,
