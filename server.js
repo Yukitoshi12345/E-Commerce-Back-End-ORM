3a120b03a46c1905092c32a68f51fa41c59d15d8
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-21 15:51:45 +1100

    server.js completed

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
+// Import the Express library to create the web application
 const express = require('express');
+// Import all routes defined in a separate file named "routes.js"
 const routes = require('./routes');
-// import sequelize connection
 
+// Import the sequelize connection from the configuration file
+const sequelize = require('./config/connection')
+
+// Create an Express application instance
 const app = express();
+// Define the port number to listen on. Use environment variable if available, otherwise default to 3001
 const PORT = process.env.PORT || 3001;
 
+// Configure middleware to parse JSON data in request body
 app.use(express.json());
+// Configure middleware to parse form data (extended: true for nested objects)
 app.use(express.urlencoded({ extended: true }));
 
+// Mount all routes imported from "routes.js"
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// Turn on connection to database and server
+sequelize.sync({ force: false }) // Set force: true to drop and recreate tables (caution!)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  });
